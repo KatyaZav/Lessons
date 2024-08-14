5c55ab3ea46d0ccc0ed37859dd2b595741ab5315
Katya Zavyalova <82714610+KatyaZav@users.noreply.github.com>
2024-08-14 10:51:08 +0500

    add task2

--- hw2/hw2/task2.cs
+++ hw2/hw2/task2.cs
@@ -7,7 +7,24 @@ namespace hw2
     {
         public static void Play()
         {
-                            
+            Random random = new Random();
+            int arraySize = random.Next(1, 101);
+
+            int[] numbersArray = new int[arraySize];
+
+            for (var i = 0; i < arraySize; i++)
+            {
+                numbersArray[i] = random.Next(10, 21);
+            }
+
+            Console.WriteLine($"Размер массива: {arraySize}");
+
+            for (var i = 0; i < numbersArray.Length; i++)
+            {
+                Console.Write($"{numbersArray[i]} ");
+            }
+
+
         }
     }
 }
